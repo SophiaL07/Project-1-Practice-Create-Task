c4fa6d276a258ce9e17f3e8044e756317d28205b
Sophia Lam <sophia@lamfamilyonline.com>
2023-09-13 22:15:38 +0000

    add function

--- college-list.py
+++ college-list.py
@@ -1,15 +1,18 @@
 public = ["UCLA", "UCSD", "UC Fullerton", "UC Berkley"]
-private = {"Stanford", "Caltech","USC"}
+private = ["Stanford", "Caltech", "USC"]
 
 def addcol(public, private):
-    y_n = input("Would you like to add a college to the list? (y/n)")
+    y_n = input("Would you like to add a college to the list? (y/n) ")
     if y_n == "y":
         new_col = input("What is the name of the new college? ")
         col_type = input("Is this college 1) public or 2) private? ")
         if col_type == "1":
-            public[new_col] = 1
-            print("Here are your public colleges: ", public )
+            public.append(new_col)
+            print("Here are your public colleges: ", public)
+            return public
         elif col_type == "2":
-            private[new_col] = 2
-            print("Here are your public colleges: ", )
-addcol()
+            private.append(new_col)
+            print("Here are your public colleges: ", private)
+            return private
+
+print("Your college list: ", addcol(public, private))
